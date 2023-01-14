986ebba0a66fad17b41a636373d60c2dd6b94bb8
ZirgomHaidar <zirgomhaidar17@gmail.com>
2023-01-14 15:14:11 +0530

    switch to oplus vibrator

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.oplus
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
